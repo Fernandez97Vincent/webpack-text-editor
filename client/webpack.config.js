8799b6e5be0ccb40d29b0f28698d4082de2b6d1a
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-28 14:46:09 -0700

    Created manifest and worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,10 +4,36 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
+    // create service worker
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js'
+    }),
+    // manifest
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'text-editor',
+      short_name: 'JATE',
+      description: 'Just Another Text Editor',
+      background_color: '#229da3',
+      theme_color: '#229da3',
+      start_url: '/',
+      publicPath: '/',
+      icons: [{
+        src: path.resolve('src/images/logo.png'),
+        // image size
+        sizes: [96, 128, 192, 256, 384, 512],
+        // join path
+        destination: path.join('assets', 'icons'),
+      }]
+    })
     mode: 'development',
     entry: {
       main: './src/js/index.js',
